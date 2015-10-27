62b1a59f14353a9f3fd2208cb7657b8a4dade46e
Jens Renken <renken@marum.de>
2015-10-27 17:19:33 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ PlaceMarker
+                                 A QGIS plugin
+ Place Marker offer a convenient way of setting placemarks in a vector layer
+                             -------------------
+        begin                : 2015-10-27
+        copyright            : (C) 2015 by Jens Renken (Marum/University of Bremen)
+        email                : renken@marum.de
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load PlaceMarker class from file PlaceMarker.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .place_marker import PlaceMarker
+    return PlaceMarker(iface)
