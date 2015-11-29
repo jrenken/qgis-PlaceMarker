500056562a7df722d06eb13bce897cc71bcd7b09
Jens Renken <jens.renken@nord-com.net>
2015-11-29 17:25:24 +0100

    change something but don't know what and why

--- placemark_layer.py
+++ placemark_layer.py
@@ -4,12 +4,20 @@ Created on 01.11.2015
 
 @author: jrenken
 '''
+from PyQt4.QtCore import pyqtSlot, QVariant
+from qgis._core import QgsVectorDataProvider, QgsField
 
 class PlaceMarkLayer:
     '''
     classdocs
     '''
 
+    __reqiredFields = [['name', QVariant.String],
+                     ['descr', QVariant.String],
+                     ['class', QVariant.String],
+                     ['timestamp', QVariant.String],
+                     ['lat', QVariant.Double],
+                     ['lon', QVariant.Double]]
 
     def __init__(self, layer):
         '''
@@ -18,17 +26,41 @@ class PlaceMarkLayer:
         :param layer: vector layer where to add the placemarks
         :type layer: QgsVectorLayer
         '''
+        self.setLayer(layer)
+
+    def setLayer(self, layer):
+        ''' assign a layer. Before check if the requirements are fulffilled
+        :param layer: a vector layer
+        '''
         if self.checkLayer(layer):
             self.layer = layer;
+            layer.layerDeleted.connect(self.layerDeleted)
+            
+            self.hasLayer = True
         else:
             self.layer = None
-
+            self.hasLayer = False
+        
     def checkLayer(self, layer):
         ''' Check if the layer geometry is point and if the 
             required attributes are available or can be added
         :param layer: vector layer where to add the placemarks
         :type layer: QgsVectorLayer
         '''
+        if layer is None:
+            return False
+        caps = layer.dataProvider().capabilities()
+        reqCaps = (QgsVectorDataProvider.AddFeatures | QgsVectorDataProvider.DeleteFeatures 
+                    | QgsVectorDataProvider.SaveAsShapefile)
+        if (caps & reqCaps) != reqCaps:
+            return False;
+        
+        missingFields = []
+        for fieldspec in self.__requiredFields:
+            try:
+                field = layer.fields().field(fieldspec[0])
+            except KeyError:
+                missingFields.append(QgsField (fieldspec[0], fieldspec[1]))
         return True
 
     def addPlaceMark(self, pos, name, description, category, timestamp):
@@ -49,5 +81,10 @@ class PlaceMarkLayer:
         :param timestamp: creation time of the placemark
         :type timestamp: string
         '''
-        pass
+        if self.hasLayer:
+            pass
         
+    @pyqtSlot()
+    def layerDeleted(self):
+        self.layer = None
+        self.hasLayer = False
